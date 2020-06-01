6fb6f84d51b3f7ade5c291c9da84edd04c29d7ea
jok <jok@localhost.localdomain>
2020-06-01 15:33:42 +0300

    week 6 day 1 exercise xp completed

---
+++ week_6/day_1/exercise_xp.py
@@ -0,0 +1,81 @@
+# uncomment to test, exercise instructions on top of all answers
+
+# Exercise 1
+# Print this in one line of code:
+
+# Hello world
+# Hello world
+# Hello world
+# Hello world
+
+# print(4 * "Hello world ")
+
+# Exercise 2
+# Calculate the result of: (99^3) * 8
+
+# print((99 ^ 3) * 8)
+
+# Exercise 3
+
+#     Create a variable called name, and give it your name as a value (text)
+#     Create a variable called age, and give it your age as a value (number)
+#     Create a variable called shoe_size, and give it your shoe size as a value
+#     Create a variable called info. Its value should be an interesting sentence about yourself, including your name, age, and shoe size. Use the variables you created earlier.
+#     Write code so that when your script is run, the info message is prompt on the screen.
+#     Run your code
+
+# name = "joel"
+# age = 29
+# shoe_size = 10
+
+# interesting_sentence = f"I'm {name}, I'm {29} years old, and I wear size {10} shoes. This isn't a very interesting sentence."
+
+# print(interesting_sentence)
+
+# Exercise 4
+
+#     Create a variable called computer_brand that contains the brand of your computer. And then insert it to a sentence like "I have a razer computer"
+
+# computer_brand = "Dell"
+# print(f"Dude you got a {computer_brand}")
+
+# this cool string interpolation thing is new in Python 3.6. I think it's pretty darn neat
+
+# Exercise 5
+
+#     Predict the output of the following code snippets:
+
+# >>> 5 < 3 will return False
+
+# >>> 3 == 3 will return True
+
+# >>> 3 == "3" will return False
+
+# >>> "3" > 3 will return a type error?
+
+# >>> "Hello" == "hello" will return False
+
+# Exercise 6
+
+#     Write a little program that asks a user for his height (in inches) and determines whether people are tall enough to ride a roller coaster or not.
+
+#         Minimum height is 35 inches
+
+# height = input("How tall are you? ")
+
+# if int(height) >= 35:
+#     print("You can ride the roller coaster")
+# else:
+#     print("Sorry kid, you're too short to ride")
+
+# Exercise 7
+
+#     Write a little program that asks the user for a number and determines whether this number is odd or even.
+
+# number = input("Gimme a number ")
+
+# if int(number) % 2 == 0:
+#     print("Even")
+# else:
+#     print("Odd")
+
