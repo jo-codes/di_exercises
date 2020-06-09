b3b477bf6d8536a768b7e82067c9aec2c7fcd6c8
jok <jok@localhost.localdomain>
2020-06-09 14:12:53 +0300

    added testing files to gitignore

---
+++ week_8/day_3/daily_challenge.py
@@ -0,0 +1,15 @@
+# Daily challenge :
+# DNA
+
+# This challenge is about Biology.
+
+#     Build a DNA object. DNA is composed of chromosomes who is itself composed of Genes.
+#         A Gene is a single value 0 or 1, it can mutate (flip).
+#         A Chromosome is a serie of 10 Genes. It also can mutate, meaning a random number of genes can randomly flip (1/2 chance to flip).
+#         A DNA is a serie of 10 chromosome, and it can also mutate the same way Chromosomes can.
+
+#     Implement these classes as you see fit.
+#     Create a new class called `Organismè that accepts a DNA object and a environement parameter that sets the probability for its DNA to mutate.
+#     Instantiate a number of Organims and let them mutate until one gets to a DNA is only made of 1s. Then stop and record the number of generations (iterations) it took.
+
+# Write your results in you personal biology research notebook and tell us your conclusion :). 
