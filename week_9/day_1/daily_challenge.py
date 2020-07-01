cca67d010b31498b5af927cb87a62d3e05c33efd
jok <jok@localhost.localdomain>
2020-07-01 22:22:20 +0300

    week 9 day 1 daily challenge - anagram checker

--- week_9/day_1/daily_challenge.py
+++ week_9/day_1/daily_challenge.py
@@ -2,3 +2,20 @@
 
 # Create a class that checks if a word is a palindrome (i.e. words that look the same written backwards).
 # For example, if the word is “radar”, it should return True.
+
+class Whydidyouaskmetomakeaclasswhenthisshouldjustbeafunction:
+    def __init__(self):
+        pass
+
+    def actualfunction(self, word):
+        if word == "".join(reversed(word)):
+            print("Anagram")
+        else:
+            print("Not anagram")
+
+
+word = input("Please pick a word to check for an anagram ")
+
+anagram_checker = Whydidyouaskmetomakeaclasswhenthisshouldjustbeafunction()
+
+anagram_checker.actualfunction(word)
