6fa1ea2014da8dbe2e0bb23f1d6a67f5b6fbd75b
jok <jok@localhost.localdomain>
2020-07-15 13:32:20 -0400

    cleaned up routes for shopping cart, renamed files for clarity

--- week_12/day_5/main.py
+++ week_12/day_5/main.py
@@ -1,31 +1,26 @@
 from flask import Flask, request, render_template, url_for, session, redirect
 import json
 
-f = open('products.json')
+f = open("products.json")
 products = json.load(f)
 
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def set_country():
-    return render_template('home.html', data=products)
+@app.route("/")
+def show_promotions():
+    return render_template("home.html", data=products)
 
 
-@app.route('/products')
+@app.route("/products")
 def list_products():
-    return render_template('products.html', products=products)
+    return render_template("products.html", products=products)
 
 
-@app.route('/home')
-def show_sales():
-    return render_template('home.html')
-
-
-@app.route('/item/<item>')
+@app.route("/item/<item>")
 def show_item(item):
-    return render_template('item.html', product=products[int(item)])
+    return render_template("item.html", product=products[int(item)])
 
 
 app.run(debug=True)
